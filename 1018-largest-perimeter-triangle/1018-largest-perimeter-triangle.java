21c6f0de4c2693c5fbcb624d966816ad56f4a465
Yashwant Mutakekar <155372378+Naruto152005@users.noreply.github.com>
2025-09-28 14:29:06 +0530

    Update 1018-largest-perimeter-triangle.java

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         int n=nums.length;
         int a,b,c;
-        for(int i=n-1;i>=2;i--)// i>=2 for avoding array out of bound;;
+        for(int i=n-1;i>=2;i--)// i>=2 for avoding array out of bound;
         {
             c=nums[i];
             a=nums[i-1];
@@ -15,3 +15,20 @@ class Solution {
         return 0;
     }
 }
+
+
+/*
+    🔹 Example
+
+Suppose n = 5. Valid indices = 0,1,2,3,4.
+
+Start with i = 4 → sides = [nums[2], nums[3], nums[4]] ✅
+
+Next i = 3 → sides = [nums[1], nums[2], nums[3]] ✅
+
+Next i = 2 → sides = [nums[0], nums[1], nums[2]] ✅
+
+Next i = 1 → sides = [nums[-1], nums[0], nums[1]] ❌ not allowed
+
+Isiliye loop ko i >= 2 tak hi chalana hai, uske baad negative index ho jaayega.
+    */
