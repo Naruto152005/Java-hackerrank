478d34429787076547ee115c3fcd95a69350756e
Yashwant Mutakekar <155372378+Naruto152005@users.noreply.github.com>
2025-10-01 19:52:43 +0530

    Time: 0 ms (100%), Space: 40.7 MB (31.78%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,5 +1,27 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
+        /*
+        this is using without maths technique
+         */
+//        int total = numBottles;  // total bottles drunk
+//        int empty = numBottles;  // empty bottles we have now
+//
+//        while (empty >= numExchange) {
+//            int newBottles = empty / numExchange;  // how many we can exchange
+//            total += newBottles;                   // drink them
+//            empty = empty % numExchange + newBottles; // leftover empties + new empties
+//        }
+//
+//        return total;
+        // this is using with math-formula;
+        //total=num+(totalbottle-1)/(exchange_value-1)/2;
+        /*
+        numBottles=15 and exchange =4
+        1)15+(15-1)/(4-1)
+        2)15+(14/3)
+        3)15+4
+        4)ans=19;
+         */
         return numBottles+(numBottles-1)/(numExchange-1);
     }
 }
