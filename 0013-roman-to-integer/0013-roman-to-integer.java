849693d4d176a6c5b4999bea8116c60e60ad5746
Yashwant Mutakekar <155372378+Naruto152005@users.noreply.github.com>
2025-10-17 20:49:16 +0530

    Time: 5 ms (59.28%), Space: 45 MB (36.34%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,36 +1,24 @@
 class Solution {
-    public int Getval(char ch )
-        {
-            switch(ch){
-                case 'I' :return 1;
-                case 'V' :return 5;
-                case 'X' :return 10;
-                case 'L' :return 50;
-                case 'C' :return 100;
-                case 'D' :return 500;
-                case 'M' :return 1000;
-                default : return 0;
-            }
-        }
     public int romanToInt(String s) {
-        int n=s.length();
+        HashMap<Character,Integer> se=new HashMap<>();
+            se.put('I',1);
+            se.put('V',5);
+            se.put('X',10);
+            se.put('L',50);
+            se.put('C',100);
+            se.put('D',500);
+            se.put('M',1000);
             int sum=0;
+            int n=s.length();
             for(int i=0;i<n;i++)
             {
                 char ch1=s.charAt(i);
-
-                if(i+1<n&& Getval(ch1)<Getval(s.charAt(i+1)))
-                    sum-=Getval(ch1);
+                int num=se.getOrDefault(ch1,0);
+                if(i+1<n&&num<se.get(s.charAt(i+1)))
+                sum-=se.get(ch1);
                 else
-                    sum+=Getval(ch1);
+                    sum+=se.get(ch1);
             }
-            /*
-            1)in this logic first it checkking that i is less then n then
-            2)after that it checking ch1 value with next ch value
-            3)if ch is less then ch+1 then add in that sum with appropriate value;
-            4)if the ch is more then ch+1 then decrease the value;
-            ------> the task is we have to implement the hashtable process less see;
-             */
             return sum;
     }
 }
