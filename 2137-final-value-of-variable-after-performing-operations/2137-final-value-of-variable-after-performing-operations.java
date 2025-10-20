48aef3908b41437dad0a4e841810c8abce96a7e5
Yashwant Mutakekar <155372378+Naruto152005@users.noreply.github.com>
2025-10-20 18:56:22 +0530

    Time: 1 ms (74.53%), Space: 43.4 MB (8.35%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -1,23 +1,34 @@
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
-        int X=0;
-        for(int  i=0;i<operations.length;i++)
-        {
-            String ch=operations[i];
-            if(Objects.equals(ch, "X++")) {
-                X = X++;
-                X++;
+        // int X=0;
+        // for(int  i=0;i<operations.length;i++)
+        // {
+        //     String ch=operations[i];
+        //     if(Objects.equals(ch, "X++")) {
+        //         X = X++;
+        //         X++;
+        //     }
+        //      if(Objects.equals(ch, "++X"))
+        //         X=++X;
+        //      if(Objects.equals(ch, "X--")) {
+        //          X = X--;
+        //          X--;
+        //      }
+        //      if(Objects.equals(ch, "--X"))
+        //         X=--X;
+        // }
+// my code;
+        // return X;
+         int X = 0;  // start with X = 0
+        
+        for (String op : operations) {
+            if (op.contains("+")) {
+                X++;   
+            } else {
+                X--;  
             }
-             if(Objects.equals(ch, "++X"))
-                X=++X;
-             if(Objects.equals(ch, "X--")) {
-                 X = X--;
-                 X--;
-             }
-             if(Objects.equals(ch, "--X"))
-                X=--X;
         }
-
+        // this is a optimized code from other 
         return X;
     }
 }
