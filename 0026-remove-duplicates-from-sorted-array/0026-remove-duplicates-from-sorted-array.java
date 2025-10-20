c779fac95fbce210e81ba41d92d5dbe430a0c27f
Yashwant Mutakekar <155372378+Naruto152005@users.noreply.github.com>
2025-10-20 10:34:53 +0530

    Time: 0 ms (100%), Space: 44.8 MB (63.02%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,18 +1,32 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int j=0;
-        int count=0;
-        for(int i=0;i<nums.length-1;i++)
-        {
-            int temp=nums[i];
-            if(nums[i+1]!=temp)
-            {
-                nums[j++]=nums[i];
-                count++;
-            }
+        // int j=0;
+        // int count=0;
+        // for(int i=0;i<nums.length-1;i++)
+        // {
+        //     int temp=nums[i];
+        //     if(nums[i+1]!=temp)
+        //     {
+        //         nums[j++]=nums[i];
+        //         count++;
+        //     }
+        // }
+        // nums[j]=nums[nums.length-1];
+        // count++;
+        // return count;
+        // this is my code but manual coding for last element;
+
+        //-------------------------------------------------------
+        // And this is from others solution
+        if (nums.length == 0) return 0;
+
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
         }
-        nums[j]=nums[nums.length-1];
-        count++;
-        return count;
+    }
+    return i + 1;
     }
 }
