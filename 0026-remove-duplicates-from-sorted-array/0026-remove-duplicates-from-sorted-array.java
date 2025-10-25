3ea0014702db8bc48248271863c66c5f4bbc24c4
Yashwant Mutakekar <155372378+Naruto152005@users.noreply.github.com>
2025-10-25 08:28:55 +0530

    Time: 1 ms (57.09%), Space: 44.8 MB (62.93%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -20,13 +20,25 @@ class Solution {
         // And this is from others solution
         if (nums.length == 0) return 0;
 
-    int i = 0;
-    for (int j = 1; j < nums.length; j++) {
-        if (nums[j] != nums[i]) {
-            i++;
-            nums[i] = nums[j];
+    // int i = 0;
+    // for (int j = 1; j < nums.length; j++) {
+    //     if (nums[j] != nums[i]) {
+    //         i++;
+    //         nums[i] = nums[j];
+    //     }
+    // }
+    // return i + 1;
+
+// this is the logic from duplicate 2 question logic if k=2 if 2 duplicate allowed in this there is only 1 allowed means no duplicate
+
+    int k=1;
+    for(int i=1;i<nums.length;i++)
+    {
+        if(nums[i]!=nums[k-1])
+        {
+            nums[k++]=nums[i];
         }
     }
-    return i + 1;
+    return k;
     }
 }
