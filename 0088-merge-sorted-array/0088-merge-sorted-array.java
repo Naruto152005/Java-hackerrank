8d04e242b46ec3242952d2bd0fb80a8197f8aec8
Yashwant Mutakekar <155372378+Naruto152005@users.noreply.github.com>
2025-11-11 20:38:01 +0530

    Time: 0 ms (100%), Space: 43.3 MB (13.55%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,9 +1,15 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1; // this is because we wanted to interate through full array 
 
-         for (int i = 0; i< n ; i++){
-            nums1[i+m] = nums2[i];
-        } 
-          Arrays.sort(nums1);
+        while(j>=0)// because we are simpply appending the largest element in the last;
+        {
+            if(i>=0&&nums1[i]>nums2[j])
+            nums1[k--]=nums1[i--];
+            else
+            nums1[k--]=nums2[j--];
+        }
     }
 }
