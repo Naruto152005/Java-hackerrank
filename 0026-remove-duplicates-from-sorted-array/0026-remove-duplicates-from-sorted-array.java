6a45f3993302a06c8f3790e93ff2afc408750e07
Yashwant Mutakekar <155372378+Naruto152005@users.noreply.github.com>
2026-07-04 15:55:14 +0530

    Time: 1 ms (78.47%), Space: 46.7 MB (42.68%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,44 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // int j=0;
-        // int count=0;
-        // for(int i=0;i<nums.length-1;i++)
-        // {
-        //     int temp=nums[i];
-        //     if(nums[i+1]!=temp)
-        //     {
-        //         nums[j++]=nums[i];
-        //         count++;
-        //     }
-        // }
-        // nums[j]=nums[nums.length-1];
-        // count++;
-        // return count;
-        // this is my code but manual coding for last element;
-
-        //-------------------------------------------------------
-        // And this is from others solution
-        if (nums.length == 0) return 0;
-
-    // int i = 0;
-    // for (int j = 1; j < nums.length; j++) {
-    //     if (nums[j] != nums[i]) {
-    //         i++;
-    //         nums[i] = nums[j];
-    //     }
-    // }
-    // return i + 1;
-
-// this is the logic from duplicate 2 question logic if k=2 if 2 duplicate allowed in this there is only 1 allowed means no duplicate
-
-    int k=1;
-    for(int i=1;i<nums.length;i++)
-    {
-        if(nums[i]!=nums[k-1])
+        int n=nums.length;
+        int j=1;
+        for(int i=0;i<n;i++)
         {
-            nums[k++]=nums[i];
+            if(nums[i]!=nums[j-1])
+            {
+                nums[j]=nums[i];
+                j++;
+            }
         }
-    }
-    return k;
+        return j;
     }
 }
