e08be8f4d2411a4d83667d1d6aa3725507db18e5
Yashwant Mutakekar <155372378+Naruto152005@users.noreply.github.com>
2026-07-10 08:59:49 +0530

    Time: 0 ms (100%), Space: 42.9 MB (99.42%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,18 +1,16 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int n=nums.length;
-        int count=0;
+        //int i=0;
         int j=0;
-        //int []arr=new int[nums.length];
+        int n=nums.length;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]!=val) {
-                nums[j] = nums[i];// swap if the condtion is satify
-                j++;// j++ to move to next index for insert elemetn(2 pointer approch);
-                count++;// number of unique element in array;
+            if(nums[i]!=val)
+            {
+                nums[j]=nums[i];
+                j++;
             }
-
         }
-        return count;
+        return j;
     }
 }
